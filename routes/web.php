389a9560a13323419839d2070723b1b10e103526
Bobby Bouwmann <bobbybouwmann@gmail.com>
2020-05-20 10:54:14 +0200

    Setup

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('exception', function () {
+    // Fill in exception here
+});
+
+Route::get('payment', function () {
+    return view('payment');
+})->name('payment');
